62014d432f9f4297c36304c1e90311c4f83a8ee4
ZaycevSergey <77642306+ZaicevSergey@users.noreply.github.com>
2023-04-11 11:25:50 +0300

    add Exception

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -25,7 +25,7 @@ public class FileReader {
                 profilDate.add(s[1]);
             }
         } catch (IOException e) {
-            System.out.println(e.getMessage());
+            System.err.println(e.getMessage());
         }
         return profilDate;
     }
