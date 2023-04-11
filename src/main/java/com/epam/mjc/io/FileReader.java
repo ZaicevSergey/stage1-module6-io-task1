202bf40a8b846b86002e080b3993f1e2f983a0fa
ZaycevSergey <77642306+ZaicevSergey@users.noreply.github.com>
2023-04-11 11:07:35 +0300

    solved

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -1,11 +1,32 @@
 package com.epam.mjc.io;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
-        return new Profile();
+        List<String> parsing = parsing(file);
+        return new Profile(parsing.get(0), Integer.parseInt(parsing.get(1)), parsing.get(2),
+                Long.parseLong(parsing.get(3)));
+    }
+
+    private List<String> parsing (File file){
+        List<String> profilDate = new ArrayList<>();
+        try (BufferedReader bufferedReader = new BufferedReader(new java.io.FileReader(file))) {
+            String line;
+
+            while ((line = bufferedReader.readLine()) != null) {
+                String[] s = line.split(" ");
+                profilDate.add(s[1]);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return profilDate;
     }
 }
